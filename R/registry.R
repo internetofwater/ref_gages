647b5c9864973bee6d116fff72e9db5cfc93115b
David Blodgett <dblodgett@usgs.gov>
2020-11-11 09:43:10 -0600

    add additional NWIS gages

--- R/registry.R
+++ R/registry.R
@@ -7,7 +7,13 @@ build_registry <- function(gage_list, registry, providers) {
     gl <- select(convert_coords(gl), provider = provider_int, provider_id)
     
     if(nrow(reg) < nrow(gl)) {
-      stop("Need to implement duplicate checks.")
+
+      gl <- dplyr::filter(gl, 
+                          !paste0(gl$provider, gl$provider_id) %in% 
+                            paste0(reg$provider, reg$provider_id))
+      
+      message(paste("Adding", nrow(gl), "to the registry."))
+      
     } else if(nrow(reg) == nrow(gl)) {
       return(reg)
     } else {
