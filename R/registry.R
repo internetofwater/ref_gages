da8c69c8022bc67e80922d6c57189200715ed7f6
David Blodgett <dblodgett@usgs.gov>
2022-03-09 20:50:40 -0600

    gage_list is not a list. fixes #20

--- R/registry.R
+++ R/registry.R
@@ -1,50 +1,44 @@
-build_registry <- function(gage_list, registry, providers) {
-  
-  gage_list <- list(gage_list)
+build_registry <- function(gl, registry, providers) {
   
   reg <- read_csv(registry)
   
-  for(gl in gage_list) {
-    gl <- left_join(gl, select(providers, provider_int = id, provider), by = "provider")
+  gl <- left_join(gl, select(providers, provider_int = id, provider), by = "provider")
+  
+  gl <- select(convert_coords(gl), provider = provider_int, provider_id)
+  
+  if(nrow(reg) == 1) {
+    message("initialize")
     
-    gl <- select(convert_coords(gl), provider = provider_int, provider_id)
+    reg$id <- as.numeric(reg$id)
+    reg$provider <- as.numeric(reg$provider)
+    gl$id <- 1000000 + c(1:nrow(gl))
     
-    if(nrow(reg) == 1) {
-      message("initialize")
-      
-      reg$id <- as.numeric(reg$id)
-      reg$provider <- as.numeric(reg$provider)
-      gl$id <- 1000000 + c(1:nrow(gl))
-      
-    } else if(nrow(reg) < nrow(gl)) {
-
-      gl <- distinct(gl)
-      
-      if(any(duplicated(gl$provider_id))) stop("found duplicate ids")
-      
-      gl <- dplyr::filter(gl, 
-                          !paste0(gl$provider, gl$provider_id) %in% 
-                            paste0(reg$provider, reg$provider_id))
-      
-      gl$id <- seq(max(reg$id), (max(reg$id) + nrow(gl) - 1))
-      
-      message(paste("Adding", nrow(gl), "to the registry."))
-      
-    } else if(nrow(reg) == nrow(gl)) {
+  } else if(nrow(reg) < nrow(gl)) {
+    
+    gl <- distinct(gl)
+    
+    if(any(duplicated(gl$provider_id))) stop("found duplicate ids")
+    
+    gl <- dplyr::filter(gl, 
+                        !paste0(gl$provider, gl$provider_id) %in% 
+                          paste0(reg$provider, reg$provider_id))
+    
+    gl$id <- seq(max(reg$id), (max(reg$id) + nrow(gl) - 1))
+    
+    message(paste("Adding", nrow(gl), "to the registry."))
+    
+  } else if(nrow(reg) == nrow(gl)) {
+    return(reg)
+  } else {
+    if(all(paste0(gl$provider, gl$provider_id) %in% 
+           paste0(reg$provider, reg$provider_id))) {
       return(reg)
     } else {
-      if(all(paste0(gl$provider, gl$provider_id) %in% 
-             paste0(reg$provider, reg$provider_id))) {
-        return(reg)
-      } else {
-        stop("gl should not be shorter than reg.")
-      }
+      stop("gl should not be shorter than reg.")
     }
-    
-    reg <- bind_rows(reg, gl)
   }
   
-  reg
+  bind_rows(reg, gl)
   
 }
 
