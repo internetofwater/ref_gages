81918d47698ee2361943ef38ec09c2d223b0d188
David Blodgett <dblodgett@usgs.gov>
2023-03-19 20:48:05 -0500

    rewrite to targets format

--- runner.R
+++ runner.R
@@ -1,13 +1,8 @@
-library(drake)
-library(dplyr)
-library(sf)
-library(nhdplusTools)
-library(dataRetrieval)
-library(sbtools)
-library(readr)
-library(knitr)
-library(mapview)
-library(sbtools)
+library(targets)
+
+tar_option_set(packages = c("nhdplusTools", "sf", "dplyr", "dataRetrieval", 
+                            "sbtools", "readr", "knitr", "mapview"),
+               memory = "transient", garbage_collection = TRUE)
 
 reference_file <- "out/ref_gages.gpkg"
 
@@ -25,84 +20,82 @@ index_dir <- "docs/"
 
 sourced <- sapply(list.files("R", pattern = "*.R$", full.names = TRUE), source)
 
-plan <- drake_plan(
+list(
   # NHDPlusV2 downloaded with nhdplusTools
-  nat_db = download_nhdplusv2("data/nhdp"),
+  tar_target("nat_db", download_nhdplusv2("data/nhdp")),
   
   # Only the network flowlines for now -- non-network could be pulled in.
-  nhdpv2_fline = read_sf(nat_db, "NHDFlowline_Network"),
-  nhdpv2_fline_proc = select(st_transform(nhdpv2_fline, 5070),
-                             COMID, REACHCODE, ToMeas, FromMeas),
-  mainstems = get_all_mainstems("data/mainstems/"),
-  vaa = get_vaa(atts = c("comid", "levelpathi"),
-                updated_network = TRUE),
+  tar_target("nhdpv2_fline", read_sf(nat_db, "NHDFlowline_Network")),
+  tar_target("nhdpv2_fline_proc", select(st_transform(nhdpv2_fline, 5070),
+                                        COMID, REACHCODE, ToMeas, FromMeas)),
+  tar_target("mainstems", get_all_mainstems("data/mainstems/")),
+  tar_target("vaa", get_vaa(atts = c("comid", "levelpathi"),
+                           updated_network = TRUE)),
   # This function downloads all NWIS sites from the site file
-  nwis_gage = get_nwis_sites(),
+  tar_target("nwis_gage", get_nwis_sites()),
   
   # This function downloads all cdec sites
-  cdec_gage = get_cdec_data(),
+  tar_target("cdec_gage", get_cdec_data()),
   
   # This functions loads locally stored streamstats sites.
-  streamstats_sites = get_streamstats_sites(),
+  tar_target("streamstats_sites", get_streamstats_sites()),
   
   # This function loads the SWIMS gage locations.
-  swims_gage = get_swim_data(),
+  tar_target("swims_gage", get_swim_data()),
   
   # this function filters and renames gage locations to a common table
-  gage_locations = get_gage_locations(nwis_gage,
-                                      streamstats_sites, 
-                                      cdec_gage),
+  tar_target("gage_locations", get_gage_locations(nwis_gage,
+                                                  streamstats_sites, 
+                                                  cdec_gage)),
   
   # This Gage layer from NHDPlusV2 is a basic starting point for
   # NWIS gage locations.
-  nhdpv2_gage = select(read_sf(nat_db, "Gage"), 
-                       nhdpv2_REACHCODE = REACHCODE, 
-                       nhdpv2_REACH_measure = Measure,
-                       nhdpv2_COMID = FLComID,
-                       provider_id = SOURCE_FEA),
+  tar_target("nhdpv2_gage", select(read_sf(nat_db, "Gage"), 
+                                   nhdpv2_REACHCODE = REACHCODE, 
+                                   nhdpv2_REACH_measure = Measure,
+                                   nhdpv2_COMID = FLComID,
+                                   provider_id = SOURCE_FEA)),
   
-  nwis_gage_hydro_locatons = get_nwis_hydrolocations(nhdpv2_gage,
-                                                     swims_gage,
-                                                     nwis_hydrolocation),
+  tar_target("nwis_gage_hydro_locatons", get_nwis_hydrolocations(nhdpv2_gage,
+                                                                 swims_gage,
+                                                                 nwis_hydrolocation)),
   
-  cdec_gage_address = get_cdec_gage_locations(cdec_gage),
+  tar_target("cdec_gage_address", get_cdec_gage_locations(cdec_gage)),
   
   # This function takes a table of all NWIS and more in the future gage
   # locations and a list of provided hydrologic locations. The provider
   # is a way to join on provider and provider_id in the all_gages input.
   # The order that hydrologic locations sources are provided will determine
   # precidence -- last defined wins.
-  gage_hydrologic_locations = get_hydrologic_locations(
+  tar_target("gage_hydrologic_locations", get_hydrologic_locations(
     all_gages = gage_locations,
     hydrologic_locations = list(
       list(provider = "https://waterdata.usgs.gov",
            locations = nwis_gage_hydro_locatons),
       list(provider = "https://cdec.water.ca.gov",
            locations = cdec_gage_address)),
-    nhdpv2_fline = nhdpv2_fline_proc),
+    nhdpv2_fline = nhdpv2_fline_proc)),
   
-  gage_hydrologic_locations_with_mainstems = add_mainstems(gage_hydrologic_locations,
-                                                           mainstems, vaa),
+  tar_target("gage_hydrologic_locations_with_mainstems", add_mainstems(gage_hydrologic_locations,
+                                                                       mainstems, vaa)),
   
   # Each entry will have a provider and provider_id that acts as a unique
   # primary key. The existing registry file will have a unique attribute
   # that contains that primary key. 
-  providers = read_csv(file_in("reg/providers.csv")),
+  tar_target("providers", read_csv(file_in("reg/providers.csv"))),
   
   
-  registry = build_registry(gage_locations,
-                            registry = file_in("reg/ref_gages.csv"),
-                            providers = providers),
+  tar_target("registry", build_registry(gage_locations,
+                                        registry = file_in("reg/ref_gages.csv"),
+                                        providers = providers)),
   
   # Creates an output for USGS namespace reference locations
-  usgs_reference_out = write_usgs_reference(gage_hydrologic_locations_with_mainstems, 
-                                            registry, providers, usgs_reference_file, 
-                                            usgs_nldi_file),
+  tar_target("usgs_reference_out", write_usgs_reference(gage_hydrologic_locations_with_mainstems, 
+                                                        registry, providers, usgs_reference_file, 
+                                                        usgs_nldi_file)),
   
-  reference_out = write_reference(gage_hydrologic_locations_with_mainstems, 
-                                  registry, providers, reference_file, 
-                                  nldi_file),
-  registry_out = write_registry(registry, "reg/ref_gages.csv"),
-  index = build_index(reference_out, index_dir))
-
-make(plan, memory_strategy = "autoclean", garbage_collection = TRUE, lock_envir = FALSE)
+  tar_target("reference_out", write_reference(gage_hydrologic_locations_with_mainstems, 
+                                              registry, providers, reference_file, 
+                                              nldi_file)),
+  tar_target("registry_out", write_registry(registry, "reg/ref_gages.csv")))
+# index" build_index(reference_out, index_dir))
